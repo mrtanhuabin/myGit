6175ffefe2be7497c16df1f8d07faa7ee1b7859d
tanhuabin <183558812@qq.com>
2021-07-16 10:17:37 +0800

    RN demo

---
+++ app/page/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+class About extends React.Component {
+  render() {
+    return (
+      <Text>
+       欢迎看到，我的地盘
+      </Text>
+    );
+  }
+}
+
+export default About;
